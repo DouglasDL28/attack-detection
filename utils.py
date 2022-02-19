22d099cf98bdec28e08991677c61e8c14f0302d7
Douglas De León <douglasdeleon@Douglass-MacBook-Pro.local>
2022-02-18 21:02:30 -0600

    changes

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import re
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, classification_report, accuracy_score
 
 nums_regex = re.compile(r"[0-9]+")
 def normalize_dst_to_src_column(value):
@@ -21,4 +22,14 @@ def is_int(val):
     except:
         return False
 
-
+def evaluate_model(true_values, preds):
+    print("Matrix de confusión: \n", confusion_matrix(true_values, preds))
+    target_names =[    "Normal flow",
+        "SYN Scan - aggressive",
+        "Denial of Service R-U-Dead-Yet",
+        "Denial of Service Slowloris"]
+    print(classification_report(true_values, preds, target_names = target_names))
+    print("recall_score: ",recall_score(true_values, preds, average=None))
+    print("precision_score: ", precision_score(true_values, preds, average=None))
+    print("f1_score: ", f1_score(true_values, preds, average=None))
+    print("accuracy: ", accuracy_score(true_values, preds))
